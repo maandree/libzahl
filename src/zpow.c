c1b7101b0f33b07c98b971b2dd075c068f319299
Mattias Andrée <maandree@kth.se>
2016-03-07 07:36:29 +0100

    A brief description of exponentiation by squaring
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- src/zpow.c
+++ src/zpow.c
@@ -8,6 +8,12 @@
 void
 zpow(z_t a, z_t b, z_t c)
 {
+	/*
+	 * Exponentiation by squaring.
+	 * 
+	 * 7↑19 = 7↑10011₂ = 7↑2⁰ ⋅ 7↑2¹ ⋅ 7↑2⁴ where a↑2↑(n + 1) = (a↑2↑n)².
+	 */
+
 	size_t i, j, n, bits;
 	zahl_char_t x;
 
