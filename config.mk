37740e679273dbf76593a8da5d44772b94f07b28
Mattias Andrée <maandree@kth.se>
2016-04-27 17:30:50 +0200

    Document UNSAFE
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- config.mk
+++ config.mk
@@ -14,6 +14,7 @@ RANLIB = ranlib
 # you have to add -DSECURE_RANDOM_PATHNAME=... to CPPFLAGS.
 # Remove -DGOOD_RAND if the higher bits have higher entropy in the lower
 # bits in rand(3), this was historically the case.
+# Add -DUNSAFE if you rather libzahl did not check for errors.
 
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -DGOOD_RAND
 CFLAGS   = -std=c99 -flto -O3 -Wall -pedantic
